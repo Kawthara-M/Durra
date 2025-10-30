66de2d66061bd341e24193e4179f35e97609b2a6
Kawthara <kawtharr.m.ahmad@gmail.com>
2025-10-30 11:24:01 +0300

    styling cards and preparing add to cart logic

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import { ThemeProvider } from "./context/ThemeContext.jsx"
 import { UserProvider } from "./context/UserContext"
+import { CartProvider } from "./context/CartContext.jsx"
 import App from "./App.jsx"
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <UserProvider>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <CartProvider>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </CartProvider>
     </UserProvider>
   </BrowserRouter>
 )
