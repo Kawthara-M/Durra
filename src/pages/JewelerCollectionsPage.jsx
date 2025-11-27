d5e191ede58d1d0aff8879d0a406390c43dad1f6
Kawthara <kawtharr.m.ahmad@gmail.com>
2025-11-27 12:14:37 +0300

    fixing collections page

--- src/pages/JewelerCollectionsPage.jsx
+++ src/pages/JewelerCollectionsPage.jsx
@@ -2,23 +2,26 @@ import { useState, useEffect } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import ProductCard from "../components/ProductCard.jsx"
 import User from "../services/api"
+import { useUser } from "../context/UserContext.jsx"
 import { fetchMetalRates } from "../services/calculator.js"
 import "../../public/stylesheets/jeweler-services.css"
 
 const JewelerCollectionsPage = () => {
   const navigate = useNavigate()
+  const { user } = useUser()
   const [collections, setCollections] = useState([])
   const [metalRates, setMetalRates] = useState()
 
   useEffect(() => {
     const getCollections = async () => {
       try {
-        const response = await User.get("/collections/")
+        const response = await User.get(`collections/shop`)
         setCollections(response.data.collections)
       } catch (err) {
         console.error("Failed to fetch collections", err)
       }
     }
+
     const loadRates = async () => {
       try {
         const rates = await fetchMetalRates()
@@ -32,39 +35,47 @@ const JewelerCollectionsPage = () => {
     getCollections()
   }, [])
 
-  return (
-    <div className="jeweler-collections">
-      <div className="jeweler-jewelry-header">
-        <h1 className="jeweler-jewelry-heading">Jewellery Collections</h1>
-        <button
-          type="button"
-          title="Add Collection"
-          onClick={() => navigate("/add-collections")}
-        >
-          Add Collection
-        </button>
-      </div>
+  if (user && user.role !== "Jeweler") {
+    return <span className="empty">You are not authorized to view this page.</span>
+  }
 
-      {collections?.length === 0 ? (
-        <p>No collections found.</p>
-      ) : (
-        <div className="grid">
-          {collections.map((collection) => (
-            <Link
-              to={`/show-collection/${collection._id}`}
-              key={collection._id}
+  return (
+    <>
+      {user?.role === "Jeweler" && (
+        <div className="jeweler-collections">
+          <div className="jeweler-jewelry-header">
+            <h1 className="jeweler-jewelry-heading">Jewelry Collections</h1>
+            <button
+              type="button"
+              title="Add Collection"
+              onClick={() => navigate("/add-collections")}
             >
-              <ProductCard
-                item={collection}
-                type="collection"
-                metalRates={metalRates}
-                showActions={false}
-              />
-            </Link>
-          ))}
+              Add Collection
+            </button>
+          </div>
+
+          {collections?.length === 0 ? (
+            <span className="empty">No Collections.</span>
+          ) : (
+            <div className="grid">
+              {collections.map((collection) => (
+                <Link
+                  to={`/show-collection/${collection._id}`}
+                  key={collection._id}
+                >
+                  <ProductCard
+                    item={collection}
+                    type="collection"
+                    metalRates={metalRates}
+                    showActions={false}
+                  />
+                </Link>
+              ))}
+            </div>
+          )}
         </div>
       )}
-    </div>
+    </>
   )
 }
 
